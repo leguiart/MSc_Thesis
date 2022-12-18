a9d5cbdc866c7810d1e1c1f2060663bdb906aa42
leguiart <luis.eguiarte@comunidad.unam.mx>
2022-12-18 00:56:03 -0600

    Make physics cache merge better

--- utils/MergePhysicsCache.py
+++ utils/MergePhysicsCache.py
@@ -1,17 +1,26 @@
 
+
 import os
+import glob
 import sys
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
-sys.path.append(os.getenv('PYTHONPATH'))# Appending repo's root dir in the python path to enable subsequent imports
+# load_dotenv()
+# sys.path.append(os.getenv('PYTHONPATH'))# Appending repo's root dir in the python path to enable subsequent imports
 
-from utils.utils import readFromJson, writeToJson
+from utils import readFromJson, writeToJson
 
-cache1 = readFromJson("experiments/physics_evaluator_cache.json")
-cache2 = readFromJson("experiments/physics_evaluator_cache2.json")
+main_cache_path = '/media/leguiart/LuisExtra'
 
-cache1.update(cache2)
 
-writeToJson("experiments/physics_evaluator_cache.json", cache1)
+if os.path.exists(main_cache_path) and os.path.isdir(main_cache_path):
+    stored_cache_paths = glob.glob(os.path.join(main_cache_path, "physics_evaluator_cache*.json"))
+    cache_result = {}
+    for cache_path in stored_cache_paths:
+        if os.path.isfile(cache_path):
+            print(cache_path)
+            cache = readFromJson(cache_path)
+            cache_result.update(cache)
+
+writeToJson(os.path.join(main_cache_path, "merged_physics_cache.json"), cache_result)
